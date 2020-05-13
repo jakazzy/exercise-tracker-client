b162bcc9812f092e740d308b912e11d50561f94c
jakazzy <jakazzy@gmail.com>
2020-05-13 11:35:14 +0000

    chore: fix prettier problems in the test file

--- src/App.js
+++ src/App.js
@@ -27,3 +27,4 @@ export default App
 // referenece for linting
 // https://thomlom.dev/setup-eslint-prettier-react/
 // https://medium.com/@uistephen/style-guides-for-linting-ecmascript-2015-eslint-common-google-airbnb-6c25fd3dff0
+// https://www.npmjs.com/package/eslint-config-prettier-standard
