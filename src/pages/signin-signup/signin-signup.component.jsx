a610f60ead71444d61c266dd9fc12d69088c8ea0
jakazzy <jakazzy@gmail.com>
2020-08-03 09:39:03 +0000

    add the sign-in-sign-up page

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './signin-signup.styles.css'
+
+function SigninSignup() {
+  return (
+    <div className="container-box">
+      <div className="divider"></div>
+    </div>
+  )
+}
+
+export default SigninSignup
