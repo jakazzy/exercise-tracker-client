5cc949196c79b9c13e271f85bdb1626fd69b48f2
jakazzy <jakazzy@gmail.com>
2020-08-03 09:39:03 +0000

    add the sign-in-sign-up page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SigninSignup from './pages/signin-signup/signin-signup.component'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <SigninSignup />
+    </div>
+  )
+}
+
+export default App
