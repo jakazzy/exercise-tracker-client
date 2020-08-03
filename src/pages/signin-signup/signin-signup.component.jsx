291877204f44b3c39f63b98407c5ab5c3571d6a8
jakazzy <jakazzy@gmail.com>
2020-08-03 11:05:48 +0000

    add the sign in page

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './signin-signup.styles.css'
+import Signin from '../../components/signin/signin.component'
 
 function SigninSignup() {
   return (
     <div className="container-box">
       <div className="divider"></div>
+      <Signin />
     </div>
   )
 }
