a392210fd92ce53cea7dfdc613d8fb570f60dab8
jakazzy <jakazzy@gmail.com>
2020-08-04 03:06:49 +0000

    set up login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import SigninSignup from './pages/signin-signup/signin-signup.component'
+import SigninPage from './pages/signin-page/signin-page.component'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <SigninSignup />
+      <Switch>
+        <Route exact path="/signup" component={SigninSignup} />
+        <Route exact path="/login" component={SigninPage} />
+      </Switch>
     </div>
   )
 }
---
+++ src/pages/signin-page/signin-page.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Signin from '../../components/signin/signin.component'
+import './signin-page.styles.css'
+
+function SigninPage() {
+  return (
+    <div className="container-box">
+      <div className="divider"></div>
+      <Signin />
+    </div>
+  )
+}
+
+export default SigninPage
