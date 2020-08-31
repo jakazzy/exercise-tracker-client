78f0159dbc7d09180a5548801c8b59649ffe0e91
jakazzy <jakazzy@gmail.com>
2020-08-31 17:10:14 +0000

    add CTA text

--- src/pages/landing-page/LandingPage.component.jsx
+++ src/pages/landing-page/LandingPage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 import HomeNavbar from '../../components/navbar/navbar.component'
 import './landing-page.styles.css'
 import Stability from '../../images/stability_ball.svg'
@@ -8,6 +9,15 @@ const LandingPage = () => {
   return (
     <div style={{ overflowX: 'hidden' }}>
       <HomeNavbar />
+      <div className="CTA-message">
+        <h1> MAKE IT HAPPEN. SHOCK EVERYONE</h1>
+        Begin your fitness goals today. Set goals and smash them goals using
+        exercise tracker.
+        <Button className="btn-bg-color-cta" type="button" block>
+          {' '}
+          JOIN NOW
+        </Button>
+      </div>
       <div className="landing-page-container">
         <div className="rounded-divider"></div>
         <div className="landing-page-images">
