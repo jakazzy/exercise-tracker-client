12da2bcf252d7d9f4b323e13fc51f03b01bf2599
jakazzy <jakazzy@gmail.com>
2020-09-01 06:53:12 +0000

    add the cards

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const CardItem = ({ width, img, info }) => {
+  return (
+    <Card style={{ width: width }} className="shadow">
+      <Card.Img
+        variant="top"
+        src={img}
+        style={{ height: '10rem', paddingTop: '1rem' }}
+      />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>{info}</Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default CardItem
--- src/pages/landing-page/LandingPage.component.jsx
+++ src/pages/landing-page/LandingPage.component.jsx
@@ -5,6 +5,8 @@ import './landing-page.styles.css'
 import Stability from '../../images/stability_ball.svg'
 import WorkOut from '../../images/working_out.svg'
 import ControlledCarousel from '../../components/carousel/carousel.component'
+import { cardData } from '../../data/data'
+import CardItem from '../../components/card/card.component'
 
 const LandingPage = () => {
   return (
@@ -32,6 +34,11 @@ const LandingPage = () => {
         <h2>SUCCESS STORIES</h2>
         <ControlledCarousel />
       </div>
+      <section className="info">
+        {cardData.map(({ width, image, id, info }) => (
+          <CardItem width={width} img={image} key={id} info={info} />
+        ))}
+      </section>
     </div>
   )
 }
