90ecf78e3d6c08f52ded9de2f44cc92f27ee9134
jakazzy <jakazzy@gmail.com>
2020-09-03 15:34:11 +0000

    add the sidebar

---
+++ src/pages/dashboard/main.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      Is it thy will, thy image should keep open My heavy eyelids to the weary
+      night? Dost thou desire my slumbers should be broken, While shadows like
+      to thee do mock my sight? Is it thy spirit that thou send'st from thee So
+      far from home into my deeds to pry, To find out shames and idle hours in
+      me, The scope and tenure of thy jealousy? O, no! thy love, though much, is
+      not so great: It is my love that keeps mine eye awake:
+    </div>
+  )
+}
+export default Main
