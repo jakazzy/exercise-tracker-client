d56661c6dd1f1fac9c8fa3d472d6593c5e66e12c
jakazzy <jakazzy@gmail.com>
2020-09-12 16:42:51 +0000

    add the progress chart

--- src/pages/dashboard/main.component.jsx
+++ src/pages/dashboard/main.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Row, Col } from 'react-bootstrap'
 import Training from '../../images/personal_training.svg'
 import DashboardNavbar from '../../components/dashboard-navbar/dashboard-navbar.component'
+import { RiPulseLine, RiFireFill, RiRunFill } from 'react-icons/ri'
+import Chart from './chart.component'
 
 import './main.styles.css'
+import ExerciseProgress from './exercise-progress.component'
 
 const Main = () => {
   return (
@@ -13,16 +16,14 @@ const Main = () => {
         className="shadow border-0"
         style={{
           position: 'relative',
-          height: '8rem',
+          height: '7rem',
           backgroundColor: '#b2d9dc'
         }}>
         <Card.Body>
           <div
             style={{
               textAlign: 'left',
-              margin: '1rem 1rem 0rem 1rem'
-              // padding: '1rem',
-              // fontSize: '1.2rem'
+              marginLeft: '2rem'
             }}>
             {' '}
             <p>
@@ -43,33 +44,47 @@ const Main = () => {
       {/* graph */}
       <Card className="shadow border-0 card-section card-section-metrics">
         <Card.Body className="metrics">
-          <div className="first-metric">
+          <div className="first-metric shadow">
             <div className="first-metric-items">
               <p>Heart Rate</p>
-              <div className="graph-hr"></div>
+              <div className="graph-hr">
+                <RiPulseLine className="graph-icon" />
+              </div>
             </div>
             <p> 71 beats per min</p>
           </div>
-          <div className="second-metric">
+          <div className="second-metric shadow">
             <div className="second-metric-items">
               <p>Calories </p>
-              <div className="graph-cal"></div>
+              <div className="graph-cal">
+                <RiFireFill className="graph-icon" />
+              </div>
             </div>
             <p> 300KCal</p>
           </div>
-          <div className="third-metric">
+          <div className="third-metric shadow">
             <div className="third-metric-items">
               <p>Distance </p>
-              <div className="graph-distance"></div>
+              <div className="graph-distance">
+                <RiRunFill className="graph-icon" />
+              </div>
             </div>
             <p> 300KCal</p>
           </div>
         </Card.Body>
       </Card>
       <Card className="shadow border-0 card-section">
-        <Card.Body className="graphs">
-          <div className="first-graph">hi</div>
-          <div className="second-graph">hello</div>
+        <Card.Body as={Row} className="graphs">
+          <Col sm={9}>
+            <div className="first-graph">
+              <Chart />
+            </div>
+          </Col>
+          <Col sm={3} className="shadow">
+            <div className="second-graph">
+              <ExerciseProgress />
+            </div>
+          </Col>
         </Card.Body>
       </Card>
     </div>
