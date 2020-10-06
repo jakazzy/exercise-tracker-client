3ca86d401a361d97a211516a80b7aa4abf11fd7c
jakazzy <jakazzy@gmail.com>
2020-10-06 21:22:48 +0000

    conditionally render auth in FE

---
+++ src/components/NavHome/navhome.component.jsx
@@ -0,0 +1,53 @@
+import React, { useContext } from 'react'
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
+import { signout } from '../../api/api'
+import { AuthContext } from '../../contexts/AuthContext'
+
+const NavHome = () => {
+  const {
+    auth: { token, isAuthenticated }
+  } = useContext(AuthContext)
+
+  const signOut = async () => {
+    await signout()
+  }
+  return (
+    <Navbar bg="light" expand="lg" className="shadow">
+      <Container>
+        <Navbar.Brand href="#home">GOALTRACKER</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Exercises</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          {!token && !isAuthenticated ? (
+            <span>
+              <Nav.Link href="/login" className="auth-link">
+                Sign in
+              </Nav.Link>
+              <Nav.Link href="/signup" className="auth-link">
+                Sign up
+              </Nav.Link>
+            </span>
+          ) : (
+            <Nav.Link onClick={signOut}>sign out</Nav.Link>
+          )}
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavHome
--- src/pages/home/home.components.jsx
+++ src/pages/home/home.components.jsx
@@ -1,44 +1,18 @@
 import React from 'react'
-import { Navbar, Nav, NavDropdown, Button, Container } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import Footer from '../../components/footer/footer'
 import { cardData } from '../../data/data'
 import CardItem from '../../components/card/card.component'
 import './home.styles.css'
+import NavHome from '../../components/NavHome/navhome.component'
+import AuthContextProvider from '../../contexts/AuthContext'
 
 const Home = () => {
   return (
     <div>
-      <Navbar bg="light" expand="lg" className="shadow">
-        <Container>
-          <Navbar.Brand href="#home">GOALTRACKER</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">About</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">
-                  Exercises
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Nav.Link href="/login">Sign in</Nav.Link>
-            <Nav.Link href="/signup" className="mr-sm-2">
-              Sign up
-            </Nav.Link>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <AuthContextProvider>
+        <NavHome />
+      </AuthContextProvider>
       <section className="content">
         <div className="call-to-action-message">
           <h1> MAKE IT HAPPEN. SHOCK EVERYONE</h1>
