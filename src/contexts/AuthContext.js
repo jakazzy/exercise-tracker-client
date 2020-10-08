b69aa1f05512e3abdcacf10517b431cb26e75e07
jakazzy <jakazzy@gmail.com>
2020-10-08 13:48:59 +0000

    make chart and progressbar data dynamic

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -13,6 +13,25 @@ const AuthContextProvider = props => {
 
   const [schedule, setSchedule] = useState([])
   const [goal, setGoal] = useState([])
+  const [chartData, setChartData] = useState([
+    { label: 'Sunday', value: '20' },
+    { label: 'Monday', value: '60' },
+    { label: 'Tuesday', value: '100' },
+    { label: 'Wednesday', value: '80' },
+    { label: 'Thursday', value: '35' },
+    { label: 'Friday', value: '15' },
+    { label: 'Saturday', value: '85' }
+  ])
+
+  const [progressData, setProgressData] = useState({
+    running: 40,
+    skipping: 20,
+    calorie: 60,
+    diet: 80,
+    heartrate: 71,
+    distance: 300,
+    calories: 300
+  })
 
   useEffect(() => {
     const userData = async () => {
@@ -23,13 +42,29 @@ const AuthContextProvider = props => {
       if (data) {
         setSchedule(data.schedule)
         setGoal(data.goal)
+        setChartData([
+          { label: 'Sunday', value: '20' },
+          { label: 'Monday', value: '60' },
+          { label: 'Tuesday', value: '100' },
+          { label: 'Wednesday', value: '80' },
+          { label: 'Thursday', value: '35' },
+          { label: 'Friday', value: '15' },
+          { label: 'Saturday', value: '85' }
+        ])
+        setProgressData({
+          distance: 40,
+          skipping: 20,
+          calories: 60,
+          diet: 80
+        })
       }
       // return status.data
     }
     userData()
   }, [])
   return (
-    <AuthContext.Provider value={{ auth, goal, schedule }}>
+    <AuthContext.Provider
+      value={{ auth, goal, schedule, chartData, progressData }}>
       {props.children}
     </AuthContext.Provider>
   )
--- src/pages/dashboard/exercise-progress.component.jsx
+++ src/pages/dashboard/exercise-progress.component.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ProgressBar } from 'react-bootstrap'
+import { AuthContext } from '../../contexts/AuthContext'
 
 const ExerciseProgress = () => {
+  const { progressData } = useContext(AuthContext)
   return (
     <div>
       <p>Running</p>
-      <ProgressBar animated variant="success" now={40} />
+      <ProgressBar animated variant="success" now={progressData.running} />
       <p>Skipping</p>
-      <ProgressBar animated variant="info" now={20} />
+      <ProgressBar animated variant="info" now={progressData.skipping} />
       <p>Calorie</p>
-      <ProgressBar animated variant="warning" now={60} />
-      <p>Abs</p>
-      <ProgressBar animated variant="danger" now={80} />
+      <ProgressBar animated variant="warning" now={progressData.calorie} />
+      <p>Diet</p>
+      <ProgressBar animated variant="danger" now={progressData.diet} />
     </div>
   )
 }
